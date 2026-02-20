34a1abb42ae36a1f2b2fd295980a353c5a709916
oriemiobang <oriemiobango@gmail.com>
2026-02-20 16:05:45 +0300

    avatar url fixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -72,7 +72,7 @@ export class UsersService {
         id: user.id,
         email: user.email,
         name: user.name,
-        imageUrl: user.profile_image_url
+        imageUrl: user.profile_image_url ?? "https://zraflqvymigtirrotmko.supabase.co/storage/v1/object/public/busy_bee_bucket/avatars/2/wanderercreative-blank-profile-picture-973460_1280.png"
     })
 
     return {accessToken: token}
